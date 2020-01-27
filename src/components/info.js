78339e32b4fab64cdcfdab1b8e7eaf3cd3081e03
cris93g <37388115+cris93g@users.noreply.github.com>
2020-01-27 01:15:29 -0600

    checking

--- src/components/info.js
+++ src/components/info.js
@@ -1,49 +1,43 @@
-import React from "react"
+import React from 'react';
 
 import {
-  FaTwitter as Twitter,
-  FaGithub as Github,
-  FaYoutube as Youtube,
-  FaInstagram as Insta,
-  FaEnvelope as Mail,
-} from "react-icons/fa"
+	FaTwitter as Twitter,
+	FaGithub as Github,
+	FaYoutube as Youtube,
+	FaInstagram as Insta,
+	FaEnvelope as Mail,
+	FaLinkedin as LinkedIn
+} from 'react-icons/fa';
 
 export default function info() {
-  return (
-    <div className="container">
-      <div className="my-5"></div>
-      <div className="display-3" style={{ color: "#FF8A00" }}>
-        <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Sumanth.
-      </div>
-      <div className="h1 code mt-4 mb-3">async {"{"}</div>
-      <div className="text-muted mx-5 my-4 h3 text-justify info">
-        A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web
-        Developer.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
-      </div>
-      <div className="h1 code mt-2 mb-3">{"}"}</div>
-      <div className="h1 mt-5">
-        <a className="mr-5 icon" href="https://twitter.com/Mr404Found">
-          <Twitter />
-        </a>
-        <a className="mr-5 icon" href="https://instagram.com/sumanth.zero7">
-          <Insta />
-        </a>
-        <a className="mr-5 icon" href="https://github.com/Mr404Found">
-          <Github />
-        </a>
-        <a
-          className="mr-5 icon"
-          href="https://www.youtube.com/channel/UCw0FDIL-HpcfzEZrjcQwhMQ"
-        >
-          <Youtube />
-        </a>
-        <a className="mr-5 icon" href="mailto://contactmr404here@gmail.com">
-          <Mail />
-        </a>
-      </div>
-    </div>
-  )
+	return (
+		<div className="container">
+			<div className="my-5" />
+			<div className="display-3" style={{ color: '#FF8A00' }}>
+				<span style={{ color: 'white' }}>Hello, I'm</span> <br />
+				Cristian.
+			</div>
+			<div className="h1 code mt-4 mb-3">async {'{'}</div>
+			<div className="text-muted mx-5 my-4 h3 text-justify info">
+				Ever since I can remember I have been in love with figuring how things work and why do they work the way
+				they do.When I turned 24 I started developing a passion for code. I went Night and Day looking
+				eventually landing in a bootcamp called DevMountain. A little bit after completing it I wanted to learn
+				more I went through MIT courses Online going through all the courses needed for BA of Computer
+				Science.Now after many struggles of impostor syndrome, I have overcome my fears and am ready to envark
+				in this journey and create great things.
+			</div>
+			<div className="h1 code mt-2 mb-3">{'}'}</div>
+			<div className="h1 mt-5">
+				<a className="mr-5 icon" href="https://www.linkedin.com/in/cristiansgonzalez/">
+					<LinkedIn />
+				</a>
+				<a className="mr-5 icon" href="https://github.com/cris93g">
+					<Github />
+				</a>
+				<a className="mr-5 icon" href="mailto://christiandevwork@gmail.com">
+					<Mail />
+				</a>
+			</div>
+		</div>
+	);
 }
